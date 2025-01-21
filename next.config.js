c2ac4846b1c3a5c7182a6b5212df80faa52ef38a
Andrey Borisov <54822691+Dusha628@users.noreply.github.com>
2025-01-21 21:39:36 +0900

    6

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
+// next.config.js
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+ 
+  basePath: '/anbor_example',
+  assetPrefix: '/anbor_example',
+
   images: {
-    unoptimized: true, // Отключаем оптимизацию для статической сборки (например, для GitHub Pages)
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
+        protocol: 'https',
+        hostname: 'images.pexels.com',
       },
     ],
   },
-  basePath: '/repository-name', // Установите имя репозитория для GitHub Pages
-  assetPrefix: '/repository-name', // Установите префикс для статических ресурсов
 };
 
 export default nextConfig;
