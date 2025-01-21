a4cd86dbd52f053c5ffe23470cdd1e54f95e6bd7
Andrey Borisov <54822691+Dusha628@users.noreply.github.com>
2025-01-21 20:47:15 +0900

    Сайт-визитка

---
+++ src/data/contact.data.ts
@@ -0,0 +1,14 @@
+export const contactInfo = [
+  {
+    title: "Почта",
+    info: "anbor766@gmail.com",
+  },
+  {
+    title: "Телефон",
+    info: "+7 962 738-69-16",
+  },
+  {
+    title: "Адрес",
+    info: "Россия, Якутск",
+  },
+];
