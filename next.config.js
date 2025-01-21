5c5962d72438925d9e21eac63b6e866b27d1f095
Andrey Borisov <54822691+Dusha628@users.noreply.github.com>
2025-01-21 21:26:00 +0900

    5

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
-/**
- * Если переменная `SKIP_ENV_VALIDATION` установлена, пропускаем валидацию переменных окружения.
- * Это полезно, например, для сборки в Docker.
- */
-if (!process.env.SKIP_ENV_VALIDATION) {
-  await import("./src/env.js").catch((err) => {
-    console.error("Ошибка импорта env.js:", err.message);
-    process.exit(1); // Завершаем процесс с кодом ошибки
-  });
-}
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    // Отключаем серверную оптимизацию изображений
-    unoptimized: true, // Требуется для хостинга на GitHub Pages
+    unoptimized: true, // Отключаем оптимизацию для статической сборки (например, для GitHub Pages)
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
   },
-  // Устанавливаем базовый путь и префикс для GitHub Pages
-  basePath: '/repository-name', // Замените на имя вашего репозитория
-  assetPrefix: '/repository-name', // Замените на имя вашего репозитория
+  basePath: '/repository-name', // Установите имя репозитория для GitHub Pages
+  assetPrefix: '/repository-name', // Установите префикс для статических ресурсов
 };
 
 export default nextConfig;
